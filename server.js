9e97a65402ba3f8cd2ed2c0afa4267cb1d245cad
Nicholas Seetaram <nicholas.daniel.seetaram@gmail.com>
2022-02-24 09:08:12 -0400

    Fix errors.

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const express = require('express');
-const { append } = require('express/lib/response');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const server = express();
 
-server.get('/', (res) => {
+server.get('/', (req, res) => {
     res.send('Hello Docker');
 });
 
